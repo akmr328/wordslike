ca01b58b2012a786ebeb0753095061806dfebdb8
akmr328 <akm328@gmail.com>
2021-03-18 18:00:08 -0400

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+       <input type="text" placeholder="Find words like..."></input>
+    
+      </header>
+    </div>
+  );
+}
+
+export default App;
